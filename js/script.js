ac275f44fbda2bc62bdad8f93e679a8fed20089f
Ryan Doiron <ryan@Ryans-MacBook-Pro.local>
2015-11-20 21:09:20 -0500

    reverted some stuff

--- js/script.js
+++ js/script.js
@@ -1,31 +1,26 @@
-if (localStorage.getItem("lastPageViewed") == false) {
-    localStorage.setItem("lastPageViewed", "step1");
-}
+var lastPageViewed = "step1";
 
 $(document).on('mobileinit', function( ) {
  $.mobile.allowCrossDomainPages = true;
  $.support.cors = true;
-});
-
-// $("#home").on('pageinit', function( ) {});
+}); 
 
 $(document).on("click", "#tutorial", function( ev ){
-    if (localStorage.getItem("lastPageViewed") != false) {
-        $.mobile.pageContainer.pagecontainer("change", "#" + localStorage.getItem("lastPageViewed"), {});
+    if (lastPageViewed != false) {
+        $.mobile.pageContainer.pagecontainer("change", "#" + lastPageViewed, {lastPageViewed});
     };
 });
 
 $(document).on("tap", "tutorial", function (ev){
-    if (localStorage.getItem("lastPageViewed") != false) {
-        $.mobile.pageContainer.pagecontainer("change", "#" + localStorage.getItem("lastPageViewed"), {});
+    if (lastPageViewed != false) {
+        $.mobile.pageContainer.pagecontainer("change", "#" + lastPageViewed, {lastPageViewed});
     };
 });
 
 $(document).on('pagechange', function (ev, data) {
     if (data.toPage.attr('id') !== "home") {
-        if (data.toPage.attr('id') !== "info") { 
-            localStorage.setItem("lastPageViewed", data.toPage.attr('id'));
+        if (data.toPage.attr('id') !== "info") {
+            lastPageViewed = data.toPage.attr('id'); 
         };
     };
 });
-
